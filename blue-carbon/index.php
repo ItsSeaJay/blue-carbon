b552e8e5de0c49b887c4d89fbc6cd46edf7a1afc
Unknown <b6010572@my.shu.ac.uk>
2017-11-24 10:58:34 +0000

    Made the basic layout for the homepage
    - Installed Showdown JS library

--- blue-carbon/index.php
+++ blue-carbon/index.php
@@ -4,8 +4,25 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project: Blue Carbon</title>
+    <link rel="stylesheet" href="styles/main.css">
   </head>
   <body>
-    <h1>Hello, World!</h1>
+    <div class="container-fluid">
+      <header>
+        <h1>Project: Blue Carbon</h1>
+        <h2>Header</h2>
+      </header>
+      <nav>
+        <ul>
+          <li>Home</li>
+        </ul>
+      </nav>
+      <div id="content">
+        <h2>Content</h2>
+      </div>
+      <footer>
+        <h2>Footer</h2>
+      </footer>
+    </div>
   </body>
 </html>
