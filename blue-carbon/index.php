7748bb88e39817f007b1de2b0a57c9b4cbabc9f6
Unknown <b6010572@my.shu.ac.uk>
2017-11-24 12:40:28 +0000

    Started working on the navigation bar

--- blue-carbon/index.php
+++ blue-carbon/index.php
@@ -4,28 +4,44 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project: Blue Carbon</title>
+    <link rel="stylesheet" href="styles/bootstrap-grid.min.css">
     <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" href="styles/navigation.css">
+    <link rel="stylesheet" href="styles/footer.css">
+    <link rel="stylesheet" href="styles/fonts.css">
   </head>
   <body>
-    <div class="page-wrapper">
-      <header>
+    <header class="header">
+      <div>
         <h1>Project: Blue Carbon</h1>
-        <h2>Header</h2>
-      </header>
-      <nav>
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-        </ul>
-      </nav>
-      <div id="content">
-        <h2>Content</h2>
-        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
+      </div>
+    </header>
+    <nav class="navigation">
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="content">
+      <div class="container">
+        <!-- Portfolio -->
+        <h1 class="text-center">Portfolio</h1>
+        <section class="row">
+          <p>
+            <input type="text" name="" value="">
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </section>
       </div>
     </div>
-    <footer>
-      <h2>Footer</h2>
+    <footer class="footer text-center">
+      <small>Copyright &copy; [name] [year] all rights reserved.</small>
     </footer>
   </body>
 </html>
---
+++ blue-carbon/sticky.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Sticky Footer</title>
+    <link rel="stylesheet" href="styles/sticky.css">
+  </head>
+  <body>
+  <div class="content">
+    <div class="content-inside">
+      content
+    </div>
+  </div>
+  <footer class="footer"></footer>
+</body>
+</html>
