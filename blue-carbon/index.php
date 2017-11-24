701626cd9273f0e642eb068f975b35842b387e35
Unknown <b6010572@my.shu.ac.uk>
2017-11-24 11:11:34 +0000

    Attempting to add a sticky footer to the page

--- blue-carbon/index.php
+++ blue-carbon/index.php
@@ -7,22 +7,25 @@
     <link rel="stylesheet" href="styles/main.css">
   </head>
   <body>
-    <div class="container-fluid">
+    <div class="page-wrapper">
       <header>
         <h1>Project: Blue Carbon</h1>
         <h2>Header</h2>
       </header>
       <nav>
         <ul>
-          <li>Home</li>
+          <li>
+            <a href="#">Home</a>
+          </li>
         </ul>
       </nav>
       <div id="content">
         <h2>Content</h2>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
       </div>
-      <footer>
-        <h2>Footer</h2>
-      </footer>
     </div>
+    <footer>
+      <h2>Footer</h2>
+    </footer>
   </body>
 </html>
