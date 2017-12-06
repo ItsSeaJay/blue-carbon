77ec688d3085fc887bd818abf70d5522b41ffdab
Unknown <b6010572@my.shu.ac.uk>
2017-12-06 17:43:22 +0000

    Started to work on a model for the user's profile

--- source/project.php
+++ source/project.php
@@ -2,7 +2,8 @@
   require_once 'models/Project_Model.php';
   require_once 'views/Project_View.php';
 
-  $model = new Project_Model();
+  $template = 'templates/project.php';
+  $model = new Project_Model($template);
   $controller = 'test';
   $view = new Project_View($model, $controller);
 
