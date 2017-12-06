e5c381061cd7e86870b3c59675b16ba07bcc7ff0
Unknown <b6010572@my.shu.ac.uk>
2017-12-06 07:51:05 +0000

    Started the project over

--- blue-carbon/red-iron/credentials.php
+++
@@ -1,9 +0,0 @@
-<?php
-  // Alter the values in this file to relate to your database
-  $credentials = array(
-    'host' => 'localhost',
-    'dbname' => 'blue_carbon',
-    'username' => 'root',
-    'password' => ''
-  );
-?>
