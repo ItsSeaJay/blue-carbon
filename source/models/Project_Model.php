85641ff9ef6cbceaf0c7a78c0af2d3ef5f28e4ef
Unknown <b6010572@my.shu.ac.uk>
2017-12-06 08:14:18 +0000

    Confirmed that red-iron imported successfully

---
+++ source/models/Project_Model.php
@@ -0,0 +1,14 @@
+<?php
+  require_once 'Model.php';
+  /**
+   * Gives access to individual projects
+   */
+  class Project_Model extends Model
+  {
+    function __construct()
+    {
+      # code...
+    }
+  }
+
+?>
