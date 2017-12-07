0810f8e2fddec501f0dd1b4ed60df04b14b57c66
Unknown <b6010572@my.shu.ac.uk>
2017-12-07 18:26:59 +0000

    Merge branch 'develop'
    - Added bootstrap styles to the project template
    - Removed the potentially copyright infringing image

--- source/templates/project.php
+++ source/templates/project.php
@@ -5,34 +5,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $project->title; ?></title>
     <!-- Styles -->
+    <link rel="stylesheet" href="styles/main.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
   </head>
   <body>
-    <!-- Title -->
-    <h1><?php echo $project->title; ?></h1>
-    <!-- Subtitle -->
-    <h2><?php echo $project->subtitle; ?></h2>
-    <!-- Thumbnail -->
-    <img src=<?php echo '"' . $project->thumbnail . '"'; ?> alt=<?php echo '"' . $project->title . ' Thumbnail"'; ?>>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <!-- Title -->
+          <h1><?php echo $project->title; ?></h1>
+          <!-- Subtitle -->
+          <h2><?php echo $project->subtitle; ?></h2>
+        </div>
+        <div class="col-lg-6">
+          <!-- Thumbnail -->
+          <img class="responsive" src=<?php echo '"' . $project->thumbnail . '"'; ?> alt=<?php echo '"' . $project->title . ' Thumbnail"'; ?>>
 
-    <!-- Description -->
-    <p><?php echo $project->description; ?></p>
+          <!-- Description -->
+          <p><?php echo $project->description; ?></p>
+        </div>
+      </div>
+    </div>
 
     <footer>
-      <!-- Copyright Notice -->
-      <small>
-        <?php
-          require_once 'models/Profile_Model.php';
+      <div class="container">
+        <!-- Copyright Notice -->
+        <small>
+          <?php
+            require_once 'models/Profile_Model.php';
 
-          $profile_model = new Profile_Model('templates/project.php');
-          $full_name = $profile_model->get_full_name();
-          $year = date("Y");
+            $profile_model = new Profile_Model('templates/project.php');
+            $full_name = $profile_model->get_full_name();
+            $year = date("Y");
 
-          $copyright = 'Copyright &copy; ' . $full_name . '&nbsp;' . $year;
+            $copyright = 'Copyright &copy; ' . $full_name . '&nbsp;' . $year;
 
-          echo $copyright;
-        ?>
-      </small>
+            echo $copyright;
+          ?>
+        </small>
+      </div>
     </footer>
   </body>
 </html>
