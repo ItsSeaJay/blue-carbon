104b2dc99dcf657b21313db8a49a76507f4596ff
Unknown <b6010572@my.shu.ac.uk>
2017-12-08 17:46:38 +0000

    Started to move login script over to AJAX

--- source/backend/index.php
+++ source/backend/index.php
@@ -6,22 +6,32 @@
     <!-- Styles -->
     <link rel="stylesheet" href="../styles/main.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <!-- Scripts -->
+    <script  src="http://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+      $(document).ready(function () {
+        $("h2").click(function () {
+          $(this).hide();
+        })
+      })
+    </script>
   </head>
   <body>
     <div class="container">
       <h1 class="text-center">Login</h1>
-      <form class="vertical-form" method="post" action="login.php">
+      <!-- Login Form -->
+      <div id="login-form">
         <label for="username">
           <h2>Username</h2>
         </label>
-        <input class="" type="text" name="username" value="" placeholder="Username">
+        <input class="form-control" type="text" name="username" value="" placeholder="Username">
         <label for="password">
           <h2>Password</h2>
         </label>
-        <input class="" type="password" name="username" value="" placeholder="Password">
-        <br>
-        <button class="btn-default" type="submit" name="login">Login</button>
-      </form>
+        <input class="form-control" type="password" name="username" value="" placeholder="Password">
+        <hr>
+        <button class="btn btn-default" type="submit" name="login">Login</button>
+      </div>
     </div>
   </body>
 </html>
