10d1fe9e575187bc82571ee3ce8a1d77a22569b9
Unknown <b6010572@my.shu.ac.uk>
2017-12-08 11:17:40 +0000

    The index file is now a login screen, the login.php file will be used to verify that login.

--- source/cms/index.php
+++ source/cms/index.php
@@ -1,5 +1,23 @@
-<?php
-  require_once '../redirect.php';
-
-  redirect('login.php');
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login to Blue Carbon CMS</title>
+    <!-- Styles -->
+    <link rel="stylesheet" href="../styles/main.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      <h1 class="text-center">Login</h1>
+      <form method="post">
+        <h2>Username</h2>
+        <input class="responsive" type="text" name="username" value="" placeholder="Username">
+        <h2>Password</h2>
+        <input class="responsive" type="password" name="username" value="" placeholder="Password">
+        <br>
+        <button type="button" name="login">Login</button>
+      </form>
+    </div>
+  </body>
+</html>
--- source/cms/login.php
+++ source/cms/login.php
@@ -1,23 +1,5 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Login to Blue Carbon CMS</title>
-    <!-- Styles -->
-    <link rel="stylesheet" href="../styles/main.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class="container">
-      <h1 class="text-center">Login</h1>
-      <form method="post">
-        <h2>Password</h2>
-        <input class="responsive" type="text" name="username" value="" placeholder="Username">
-        <h2>Password</h2>
-        <input class="responsive" type="text" name="username" value="" placeholder="Username">
-        <br>
-        <button type="button" name="login">Login</button>
-      </form>
-    </div>
-  </body>
-</html>
+<?php
+  include '../redirect.php';
+
+  redirect('index.php');
+?>
