585a2ac652a38df0d32196b650591d386a0d80b0
Unknown <b6010572@my.shu.ac.uk>
2017-12-13 13:56:05 +0000

    Added OpenIconic to the list of dependancies

--- source/backend/templates/dashboard.php
+++ source/backend/templates/dashboard.php
@@ -6,6 +6,8 @@
     <!-- Styles -->
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <!-- Open Iconic -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" integrity="sha256-BJ/G+e+y7bQdrYkS2RBTyNfBHpA9IuGaPmf9htub5MQ=" crossorigin="anonymous">
     <!-- Scripts -->
     <!-- JQuery -->
     <script  src="http://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
@@ -24,8 +26,8 @@
       <!-- Right Side -->
       <ul class="nav navbar-nav navbar-right">
         <li class="navbar-item">
-          <a href="logout.php">
-            Logout
+          <a class="nav-link" href="logout.php">
+            <span class="oi oi-account-logout"></span>&nbsp;Logout
           </a>
         </li>
       </ul>
