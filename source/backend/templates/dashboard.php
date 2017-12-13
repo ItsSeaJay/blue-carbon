2f31341ba8624f680f2347a2892a972b54365b72
Unknown <b6010572@my.shu.ac.uk>
2017-12-13 15:18:30 +0000

    Continued to work with sidebar

--- source/backend/templates/dashboard.php
+++ source/backend/templates/dashboard.php
@@ -22,7 +22,7 @@
       <!-- Top Navigation Bar -->
       <nav class="navbar navbar-dark bg-dark">
         <!-- Brand -->
-        <a class="navbar-brand" href="dashboard.php">Blue Carbon</a>
+        <a class="navbar-brand" href="dashboard.php">Aquafolio</a>
         <!-- Left Side -->
         <ul class="nav navbar-nav navbar-left">
 
@@ -38,47 +38,39 @@
       </nav>
     </header>
 
-    <div id="wrapper">
+    <div id="wrapper" class="toggled">
       <!-- Sidebar -->
       <div id="sidebar-wrapper">
-          <ul class="sidebar-nav">
-              <li class="sidebar-brand">
-                  <a href="#">
-                      Start Bootstrap
-                  </a>
-              </li>
-              <li>
-                  <a href="#">Dashboard</a>
-              </li>
-              <li>
-                  <a href="#">Shortcuts</a>
-              </li>
-              <li>
-                  <a href="#">Overview</a>
-              </li>
-              <li>
-                  <a href="#">Events</a>
-              </li>
-              <li>
-                  <a href="#">About</a>
-              </li>
-              <li>
-                  <a href="#">Services</a>
-              </li>
-              <li>
-                  <a href="#">Contact</a>
-              </li>
-          </ul>
+        <ul class="sidebar-nav">
+          <li>
+            <a href="#">Dashboard</a>
+          </li>
+          <li>
+            <a href="#">Shortcuts</a>
+          </li>
+          <li>
+            <a href="#">Overview</a>
+          </li>
+          <li>
+            <a href="#">Events</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
       </div>
       <!-- /#sidebar-wrapper -->
 
       <!-- Page Content -->
       <div id="page-content-wrapper">
           <div class="container-fluid">
-              <h1>Simple Sidebar</h1>
-              <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
-              <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
-              <a href="#menu-toggle" class="btn btn-secondary" id="menu-toggle">Toggle Menu</a>
+            <a href="#menu-toggle" class="btn btn-primary" id="menu-toggle">Toggle Menu</a>
           </div>
       </div>
       <!-- /#page-content-wrapper -->
