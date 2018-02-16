670942c1468bc4d2e519326f3d0602abb3d6a93e
Unknown <b6010572@my.shu.ac.uk>
2018-02-16 21:13:51 +0000

    Changed theme to Read the Docs

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -83,7 +83,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -186,5 +186,3 @@ epub_copyright = copyright
 
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
-
-
